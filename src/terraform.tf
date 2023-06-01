dd2abcdf26c39df56b9a8d24ec33c22968857457
sbc-juanarevalo <juanarevalo@subocol.com>
2023-06-01 06:33:02 -0500

    Commit 22

--- src/terraform.tf
+++ src/terraform.tf
@@ -1,10 +1,14 @@
-provider "aws" {
-    region = "us-east-1"
-}
-resource "aws_instance" "instance1" {
-    ami = "ami-0889a44b331db0194"
-    instance_type = "t1.micro"
-    tags = {
-      "name" = "first_instance"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
     }
+  }
+
+  backend "s3" {
+    key    = "terraform/orbika/tfstate"
+    region = "us-east-1"
+  }
+
 }
