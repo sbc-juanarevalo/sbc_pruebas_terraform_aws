8714a0df8d39b20b562eb21c2c2ebc9fba3ae46e
sbc-juanarevalo <juanarevalo@subocol.com>
2023-06-16 08:34:13 -0500

    Commit 57

--- src/main.tf
+++ src/main.tf
@@ -11,9 +11,9 @@ module "s3" {
     vars3_branch = var.branch_name
 }
 
-/*
+
 module "cloudfront" {
     source = "./modules/sbc-module-cloudfront"
     varcf_bucket_regional_domain_name = module.s3.vars3_bucket_regional_domain_name
   
-}*/
+}
